12a9fcf8ab54d7e40d6bb1ceb753d5729ac4d67c
jayoharedee <jordan@caxap.design>
2019-08-27 15:27:57 -0400

    created top nav component

---
+++ src/js/components/topnav/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './topnav.scss'
+
+const TopNav = () => (
+  <nav id="nav-menu">
+    <a href="#projects">PROJECTS</a>
+    <a href="#services">SERVICES</a>
+    <a href="#logo" id="logo">
+      Ceremony
+    </a>
+    <a href="#about">ABOUT</a>
+    <a href="#contact">CONTACT</a>
+  </nav>
+)
+
+export default TopNav;
