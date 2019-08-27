f50dcb5f89e7438fd1ccd052519d0cd81c63e152
jayoharedee <jordan@caxap.design>
2019-08-27 18:41:56 -0400

    brought in project component

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
 
 import TopNav from './topnav'
+import Projects from './projects'
 
 const App = () => {
   return (
-    <TopNav />
+    <>
+      <TopNav />
+      <Projects />
+    </>
   )
 }
 
