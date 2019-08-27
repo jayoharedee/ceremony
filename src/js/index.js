affc5e538c05863cf86e82b584c23de857097c0a
jayoharedee <jordan@caxap.design>
2019-08-27 18:41:46 -0400

    removed comments

--- src/js/index.js
+++ src/js/index.js
@@ -5,19 +5,9 @@ import App from './components/App'
 
 import '../css/fonts.css'
 import '../css/style.css'
-import '../css/desktop.css'
+// import '../css/desktop.css'
 
 
 render(<App />, document.getElementById('app'))
 
 module.hot.accept();
-// function component() {
-//     const element = document.createElement('div');
-
-//     // Lodash, currently included via a script, is required for this line to work
-//     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-//     return element;
-// }
-
-// document.body.appendChild(component());
