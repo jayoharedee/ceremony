17f4338089169f6e66494d9951c94c1278507d61
jayoharedee <jordan@caxap.design>
2019-08-27 18:40:55 -0400

    linted

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 const webpack = require('webpack')
-const path = require('path');
-const HtmlWebPackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const devMode = process.env.NODE_ENV !== 'production';
+const path = require('path')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const devMode = process.env.NODE_ENV !== 'production'
 
 module.exports = {
   entry: './src/js/index.js',
