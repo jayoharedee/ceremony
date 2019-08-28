905670490820e05a571145c42f71717411f113ba
jayoharedee <jordan@caxap.design>
2019-08-28 19:07:20 -0400

    mock db and temp startup at a service layer

---
+++ src/js/services/api/provider/Request.js
@@ -0,0 +1,55 @@
+import * as React from 'react'
+import axios from 'axios'
+import isEqual from 'lodash.isequal'
+
+class Axios extends React.Component {
+  constructor(props) {
+    super(props)
+
+    // NOTE: Do not use this.context as React uses it internally
+    this.axiosContext = React.createContext(this.value)
+  }
+
+  axiosRequest() {
+    const { config } = this.props
+
+    // Reset the request
+    this.value = null
+    this.forceUpdate()
+
+    axios
+      .request(config)
+      .then(response => {
+        this.value = response
+        this.forceUpdate()
+      })
+      .catch(error => {
+        this.value = error
+        this.forceUpdate()
+      })
+  }
+
+  componentDidMount() {
+    this.axiosRequest()
+  }
+
+  componentDidUpdate(prevProps) {
+    if (!isEqual(this.props.config, prevProps.config)) {
+      this.axiosRequest()
+    }
+  }
+
+  render() {
+    const { children } = this.props
+
+    const { Provider, Consumer } = this.axiosContext
+
+    return (
+      <Provider value={this.value}>
+        <Consumer>{children}</Consumer>
+      </Provider>
+    )
+  }
+}
+
+export default Axios
