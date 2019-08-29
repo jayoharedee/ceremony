22553162ce6c80f453d53b22b718f7e424864802
jayoharedee <jordan@caxap.design>
2019-08-28 20:09:39 -0400

    Included the request component

--- src/js/components/projects/index.js
+++ src/js/components/projects/index.js
@@ -3,26 +3,45 @@ import React from 'react'
 import './style.scss'
 
 // populate projects with API call for data
-import { projects } from './data'
-
+// import { projects } from './data'
+import Request from '../../services/api/provider/Request'
+// change this for network call
 import ProjectItem from './project-item'
 
-// barrel role and map images to create new sections
-// import AppraisalCard from './img/Appraisal Card Mockup.png'
+const config = {
+  method: 'get',
+  pathname: '/projects'
+}
 
 const Projects = () => (
-  <main id="projects" role="main">
-    {projects.map(project => (
-      <ProjectItem
-        key={project.id}
-        alt={project.img.alt}
-        img={project.img.src}
-        heading={project.heading}
-        type={project.type}
-        client={project.client}
-      />
-    ))}
-  </main>
+  <Request call={config}>
+    {response => {
+      if (response instanceof Error) {
+        return <div>{response.message}</div>
+      }
+
+      if (!response) {
+        return <div>loading ...</div>
+      }
+
+      const projects = response.data
+
+      return (
+        <main id="projects" role="main">
+          {projects.map(project => (
+            <ProjectItem
+              key={project.id}
+              alt={project.img.alt}
+              img={project.img.src}
+              heading={project.heading}
+              type={project.type}
+              client={project.client}
+            />
+          ))}
+        </main>
+      )
+    }}
+  </Request>
 )
 
 export default Projects
