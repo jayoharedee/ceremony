d8757e539f3ead1522e5d0f008efd286a6e1b35e
jayoharedee <jordan@caxap.design>
2019-09-06 02:05:24 -0400

    debounce file

---
+++ src/js/utils/debounce.js
@@ -0,0 +1,22 @@
+export function debounce(func, wait, immediate) {
+  let timeout
+  let initialArgs
+  return function() {
+    let context = this,
+      args = arguments
+    if (!timeout) {
+      initialArgs = args
+      if (immediate) {
+        func.apply(context, args)
+      }
+    }
+    clearTimeout(timeout)
+    timeout = setTimeout(function() {
+      timeout = null
+      if (!immediate || (immediate && initialArgs != args)) {
+        func.apply(context, args)
+      }
+        
+    }, wait)
+  }
+}
