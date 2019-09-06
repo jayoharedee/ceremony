528ace2f75829642c391369103cbb473cf3bc417
jayoharedee <jordan@caxap.design>
2019-09-06 02:29:38 -0400

    include router

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,14 +1,22 @@
 import React from 'react'
+import { BrowserRouter as Router, Route} from 'react-router-dom'
+
 
 import TopNav from './topnav'
 import Projects from './projects'
+import Services from './company-services'
+import About from './about'
 
 const App = () => {
   return (
-    <>
-      <TopNav />
-      <Projects />
-    </>
+    <Router>
+      <>
+        <TopNav />
+        <Route path="/" exact component={Projects} />
+        <Route path="/services/" component={Services} />
+        <Route path="/about/" component={About} />
+      </>
+    </Router>
   )
 }
 
